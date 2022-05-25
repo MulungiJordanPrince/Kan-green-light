1d42ef53b30690cffa5bbc4e35b9c145e10cfe18
Kanyesigye2002 <46765918+Kanyesigye2002@users.noreply.github.com>
2022-05-25 17:25:50 +0300

    Update SocialsButton.js

--- src/components/SocialsButton.js
+++ src/components/SocialsButton.js
@@ -26,19 +26,19 @@ export default function SocialsButton({ initialColor = false, simple = true, lin
       name: 'Instagram',
       icon: 'ant-design:instagram-filled',
       socialColor: '#E02D69',
-      path: '#https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbE41ZXlCRFVPOEQ0MEJTa3FEdklBaTBGSGM0UXxBQ3Jtc0ttVzNqLXd6NXpSeGdUcDdQWjV3N193V0F0aFdzXzczajBibXdBbTNxR1RHNDhXejViMnpLY2x2Q0swMThBdk1uUHFvNk5pX1VRNmZyYW9leEhmYzhGU1JTYlJHSEc0RnZ2RGVGTVFtOHR6bDIyZFEtOA&q=https%3A%2F%2Fwww.instagram.com%2Fgloriouschurchofchristug%2F',
+      path: 'https://www.instagram.com/gloriouschurchofchristug/',
     },
     {
       name: 'Youtube',
-      icon: 'eva:youtube-fill',
-      socialColor: '#007EBB',
+      icon: 'ant-design:youtube-filled',
+      socialColor: '#dc0404',
       path: '#https://www.youtube.com/channel/UCnSTVCj5cCr0XIGUFQu1yqw?sub_confirmation=1',
     },
     {
       name: 'Twitter',
       icon: 'eva:twitter-fill',
       socialColor: '#00AAEC',
-      path: 'https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbmJxZWRSSkU0TDhEYXc3Z09uZGFnVmJzRC1nUXxBQ3Jtc0ttMlFHbjNaSlU0VW5EMFVDWlYwQWU2cXNRZUE5dE9CM1pZZXFLcU9fTnNCZlNzN1hEM1BRYmh1RG8ySy1OMDRMQWx1US1vMzJlcHFOT09kZ2swQ05WUzhiQzZibkZFZHg0N3UwRV9TeUFpREQwcjczWQ&q=https%3A%2F%2Ftwitter.com%2Fgloriouschug',
+      path: 'https://twitter.com/gloriouschug',
     },
   ];
 
