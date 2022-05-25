d46805b14f3eab348a260d5b1b2e9f3d92d79619
Kanyesigye2002 <46765918+Kanyesigye2002@users.noreply.github.com>
2022-05-25 17:09:47 +0300

    Update SocialsButton.js

--- src/components/SocialsButton.js
+++ src/components/SocialsButton.js
@@ -20,25 +20,25 @@ export default function SocialsButton({ initialColor = false, simple = true, lin
       name: 'FaceBook',
       icon: 'eva:facebook-fill',
       socialColor: '#1877F2',
-      path: links.facebook || '#facebook-link',
+      path: '#https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbVRYLTdPRUNhVmhqeTh5d1VqQnJKVDBhelVoQXxBQ3Jtc0tsWk4xeFJQbWpqZkd5TlRrQzFOTmU0dTZleEVtOV9sS0hCeHBjcVpienJPSDJ2RlpQSkFVUWtxS2VndTRSUWVtd3doYnkycE84RU94ay1fYkZMSnY1cFZtQ2gyd1dhQ0xNSWRzUFphZkJ0VVJWMGVEcw&q=https%3A%2F%2Fwww.facebook.com%2FGloriousChurchOfChristMinistries',
     },
     {
       name: 'Instagram',
       icon: 'ant-design:instagram-filled',
       socialColor: '#E02D69',
-      path: links.instagram || '#instagram-link',
+      path: '#https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbE41ZXlCRFVPOEQ0MEJTa3FEdklBaTBGSGM0UXxBQ3Jtc0ttVzNqLXd6NXpSeGdUcDdQWjV3N193V0F0aFdzXzczajBibXdBbTNxR1RHNDhXejViMnpLY2x2Q0swMThBdk1uUHFvNk5pX1VRNmZyYW9leEhmYzhGU1JTYlJHSEc0RnZ2RGVGTVFtOHR6bDIyZFEtOA&q=https%3A%2F%2Fwww.instagram.com%2Fgloriouschurchofchristug%2F',
     },
     {
-      name: 'Linkedin',
-      icon: 'eva:linkedin-fill',
+      name: 'Youtube',
+      icon: 'eva:youtube-fill',
       socialColor: '#007EBB',
-      path: links.linkedin || '#linkedin-link',
+      path: '#https://www.youtube.com/channel/UCnSTVCj5cCr0XIGUFQu1yqw?sub_confirmation=1',
     },
     {
       name: 'Twitter',
       icon: 'eva:twitter-fill',
       socialColor: '#00AAEC',
-      path: links.twitter || '#twitter-link',
+      path: 'https://www.youtube.com/redirect?event=channel_banner&redir_token=QUFFLUhqbmJxZWRSSkU0TDhEYXc3Z09uZGFnVmJzRC1nUXxBQ3Jtc0ttMlFHbjNaSlU0VW5EMFVDWlYwQWU2cXNRZUE5dE9CM1pZZXFLcU9fTnNCZlNzN1hEM1BRYmh1RG8ySy1OMDRMQWx1US1vMzJlcHFOT09kZ2swQ05WUzhiQzZibkZFZHg0N3UwRV9TeUFpREQwcjczWQ&q=https%3A%2F%2Ftwitter.com%2Fgloriouschug',
     },
   ];
 
